7d0503292b48a538a5dbb9708449f97ef04e4cc5
rishigoel2003 <rishigoel25@gmail.com>
2025-08-21 19:40:08 +1000

    more UI with balls and title screen

--- title_screen.gd
+++ title_screen.gd
@@ -1,8 +1,24 @@
 extends Control
 
+var ball_scene = preload("res://Ball.tscn")
+
+
 func _ready():
 	# Connect the begin button
 	$BeginButton.pressed.connect(_on_begin_button_pressed)
+	
+	for i in 10:
+		
+		var new_ball = ball_scene.instantiate()
+		new_ball.z_index = -1 
+			
+		# Position it randomly within the walls
+		var random_x = randf_range(-3000, 3000)
+		var random_y = randf_range(-2800, 1800)
+		new_ball.position = Vector2(random_x, random_y)
+		
+		add_child(new_ball)
+			
 
 func _on_begin_button_pressed():
 	# Switch to the main game scene
--- win_screen.gd
+++ win_screen.gd
@@ -1,8 +1,23 @@
 extends Control
 
+var ball_scene = preload("res://Ball.tscn")
+
+
 func _ready():
 	# Connect the play again button
 	$PlayAgainButton.pressed.connect(_on_play_again_pressed)
+	
+	for i in 10:
+		
+		var new_ball = ball_scene.instantiate()
+		new_ball.z_index = -1 
+			
+		# Position it randomly within the walls
+		var random_x = randf_range(-3000, 3000)
+		var random_y = randf_range(-2800, 1800)
+		new_ball.position = Vector2(random_x, random_y)
+		
+		add_child(new_ball)
 
 func _on_play_again_pressed():
 	# Go back to title screen
